8b43715abd840da50ef2e586de85c85a11f3cca1
Jeff Yang <jeffjieyang@gmail.com>
2020-03-28 01:42:59 -0700

    init commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jeffjyang/go-webhook
+
+go 1.13
---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+)
+
+var COMMAND = "./script.sh"
+
+func webhookAsync(res http.ResponseWriter, _ *http.Request) {
+	log.Print("Endpoint: /webhook/async")
+
+	err := exec.Command(COMMAND).Start()
+
+	if err != nil {
+		fmt.Fprint(res, "Error triggering script: " + err.Error())
+		return
+	}
+	fmt.Fprint(res, "Triggered command: " + COMMAND)
+}
+
+func webhook(res http.ResponseWriter, req *http.Request) {
+	log.Print("Endpoint: /webhook")
+
+	out, err := exec.Command(COMMAND).Output()
+
+	if err != nil {
+		fmt.Fprint(res, "\nAn error occurred:\n" + err.Error())
+	}
+	if out != nil && len(out) > 0 {
+		fmt.Fprint(res, "\nCommand output:\n" + string(out))
+	}
+}
+
+func main() {
+	port := "8080"
+
+	args := os.Args[1:]
+	if len(args) > 0 {
+		if len(args) == 1 {
+			port = args[0]
+		} else if len(args) == 2 {
+			port = args[0]
+			COMMAND = args[1]
+		} else {
+			log.Fatal("Usage: TODO") // TODO
+		}
+	}
+
+	log.Print("Starting server on port " + port)
+
+	http.HandleFunc("/webhook/", webhook)
+	http.HandleFunc("/webhook/async/", webhookAsync)
+
+	log.Fatal(http.ListenAndServe(":" + port, nil))
+}
