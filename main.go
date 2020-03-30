c1bcafc0ef11a3f4b450345412c75f4743d96c9b
Jeff Yang <jeffjieyang@gmail.com>
2020-03-30 03:40:06 -0700

    add otp authentication

--- main.go
+++ main.go
@@ -1,29 +1,59 @@
 package main
 
 import (
+	"crypto/hmac"
+	"crypto/sha1"
+	"encoding/base32"
+	"encoding/binary"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os/exec"
+	"strconv"
+	"time"
 )
 
 var COMMAND = "./script.sh"
+var OTP_SECRET []byte
 
-func webhookAsync(res http.ResponseWriter, _ *http.Request) {
-	log.Print("Endpoint: /webhook/async")
+func main() {
+	portPtr := flag.String("port", "8080", "Server port")
+	commandPtr := flag.String("command", COMMAND, "Shell command to execute")
+	secret := flag.String("otp-secret", "", "Base32 encoded secret code if you wish to enable OTP authentication")
 
-	err := exec.Command(COMMAND).Start()
+	flag.Parse()
 
-	if err != nil {
-		fmt.Fprint(res, "Error triggering script: " + err.Error())
-		return
+	COMMAND = *commandPtr
+
+	if *secret != "" {
+		var err error
+		OTP_SECRET, err = base32.StdEncoding.DecodeString(*secret)
+		if err != nil {
+			log.Fatal("Unable to parse OTP secret:\n" + err.Error())
+		}
 	}
-	fmt.Fprint(res, "Triggered command: " + COMMAND)
+
+	if OTP_SECRET == nil {
+		log.Print("OTP authentication disabled")
+	} else {
+		log.Print("OTP authentication enabled")
+	}
+	log.Print("Starting server on port " + *portPtr)
+
+	http.HandleFunc("/webhook/", webhook)
+	http.HandleFunc("/webhook/async/", webhookAsync)
+
+	log.Fatal(http.ListenAndServe(":" + *portPtr, nil))
 }
+
 
 func webhook(res http.ResponseWriter, req *http.Request) {
 	log.Print("Endpoint: /webhook")
+	if !authenticateRequest(req) {
+		fmt.Fprint(res, "Unauthorized.")
+		return
+	}
 
 	out, err := exec.Command(COMMAND).Output()
 
@@ -35,17 +65,54 @@ func webhook(res http.ResponseWriter, req *http.Request) {
 	}
 }
 
-func main() {
-	portPtr := flag.String("port", "8080", "Server port")
-	commandPtr := flag.String("command", COMMAND, "Shell command to execute")
+func webhookAsync(res http.ResponseWriter, req *http.Request) {
+	log.Print("Endpoint: /webhook/async")
+	if !authenticateRequest(req) {
+		fmt.Fprint(res, "Unauthorized.")
+		return
+	}
 
-	flag.Parse()
+	err := exec.Command(COMMAND).Start()
 
-	COMMAND = *commandPtr
-	log.Print("Starting server on port " + *portPtr)
+	if err != nil {
+		fmt.Fprint(res, "Error triggering script: " + err.Error())
+		return
+	}
+	fmt.Fprint(res, "Triggered command: " + COMMAND)
+}
 
-	http.HandleFunc("/webhook/", webhook)
-	http.HandleFunc("/webhook/async/", webhookAsync)
+func authenticateRequest(req *http.Request) bool {
+	if OTP_SECRET == nil {
+		// otp authentication disabled
+		return true
+	}
+	return verifyOtp(req.URL.Query().Get("otp"))
+}
 
-	log.Fatal(http.ListenAndServe(":" + *portPtr, nil))
+func verifyOtp(otp string) bool {
+	expected := generateOtp()
+	return otp == expected
+}
+
+// never roll your own crypto, but for the sake of keeping this project simple, this will do.
+func generateOtp() string {
+	curTime := time.Now().Unix() / 30
+	message := make([]byte, 8)
+	binary.BigEndian.PutUint64(message, uint64(curTime))
+
+	hmacSha1 := hmac.New(sha1.New, OTP_SECRET)
+	hmacSha1.Write(message)
+	hash := hmacSha1.Sum(nil)
+
+	offset := hash[len(hash) - 1] & 0b1111
+	truncatedHash := hash[offset : offset + 4]
+
+	code := binary.BigEndian.Uint32(truncatedHash)
+	code = (code & 0x7fffffff) % 1000000
+
+	otp := strconv.FormatInt(int64(code), 10)
+	for len(otp) < 6 {
+		otp = "0" + otp
+	}
+	return otp
 }
