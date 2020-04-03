fcee36eee25a9745e0ff50532bab3a4cc852daf8
Jeff Yang <jeffjieyang@gmail.com>
2020-04-02 22:08:45 -0700

    allow multiple scripts

--- main.go
+++ main.go
@@ -14,53 +14,30 @@ import (
 	"os"
 	"os/exec"
 	"strconv"
+	"strings"
 	"time"
 )
 
-var SCRIPT string
-var OTP_SECRET []byte
-
-var OUTPUT_LOG_FILE = "output.log"
+var OtpSecret []byte
 
 func main() {
 	portPtr := flag.String("port", "8080", "Server port")
-	scriptPtr := flag.String("script", "script.sh", "Shell script to execute")
 	secret := flag.String("otp-secret", "",
-		"Base32 encoded secret code if you wish to enable OTP authentication. Set this value if you wish to enable OTP authentication.")
+		"Base32 encoded secret code for OTP authentication. Set this value only if you wish to enable OTP authentication.")
 
 	flag.Parse()
 
-	// check file permissions on the shell script
-	script := *scriptPtr
-	info, err := os.Stat(script)
-	if err != nil {
-		log.Fatal("An error occurred:\n" + err.Error())
-	}
-	mode := info.Mode()
-	if mode&0100 == 0 {
-		log.Fatal("Script " + script + " is not executable. Double check its file permissions.")
-	}
-
-	SCRIPT = "./" + script
-
 	if *secret != "" {
 		var err error
-		OTP_SECRET, err = base32.StdEncoding.DecodeString(*secret)
+		OtpSecret, err = base32.StdEncoding.DecodeString(*secret)
 		if err != nil {
 			log.Fatal("Unable to parse OTP secret:\n" + err.Error())
 		}
 	}
-	if OTP_SECRET == nil {
+	if OtpSecret == nil {
 		log.Print("OTP authentication disabled")
 	} else {
 		log.Print("OTP authentication enabled")
-	}
-
-	if _, err := os.Stat(OUTPUT_LOG_FILE); os.IsNotExist(err) {
-		_, fileErr := os.Create(OUTPUT_LOG_FILE)
-		if fileErr != nil {
-			log.Fatal("Unable to create log file:\n" + fileErr.Error())
-		}
 	}
 
 	log.Print("Starting server on port " + *portPtr)
@@ -72,38 +49,47 @@ func main() {
 	log.Fatal(http.ListenAndServe(":" + *portPtr, nil))
 }
 
-
 func webhook(res http.ResponseWriter, req *http.Request) {
-	log.Print("Endpoint: /webhook")
+	log.Print("Endpoint: /webhook/")
 	if !authenticateRequest(req) {
 		fmt.Fprint(res, "Unauthorized.")
 		return
 	}
+	scriptFile, err := parseScriptFile(req.URL.Path, "/webhook/")
+	if err != nil {
+		fmt.Fprint(res, "An error occurred:\n\n" + err.Error())
+		return
+	}
 
-	out, err := exec.Command(SCRIPT).Output()
-	go writeLog(out)
+	out, err := exec.Command("/bin/bash", scriptFile).Output()
+	go writeLog(scriptFile, out, err)
 
 	if err != nil {
 		fmt.Fprint(res, "An error occurred:\n\n" + err.Error())
 	}
 	if out != nil && len(out) > 0 {
-		fmt.Fprint(res, "Script output:\n\n" + string(out))
+		fmt.Fprint(res, "\n\nScript output:\n\n" + string(out))
 	}
 }
 
 func webhookAsync(res http.ResponseWriter, req *http.Request) {
-	log.Print("Endpoint: /webhook/async")
+	log.Print("Endpoint: /webhook/async/")
 	if !authenticateRequest(req) {
 		fmt.Fprint(res, "Unauthorized.")
 		return
 	}
+	scriptFile, err := parseScriptFile(req.URL.Path, "/webhook/async/")
+	if err != nil {
+		fmt.Fprint(res, "An error occurred:\n\n" + err.Error())
+		return
+	}
 
 	go func() {
-		out, _ := exec.Command(SCRIPT).Output()
-		writeLog(out)
+		out, scriptErr := exec.Command("/bin/bash", scriptFile).Output()
+		writeLog(scriptFile, out, scriptErr)
 	}()
 
-	fmt.Fprint(res, "Triggered script: " + SCRIPT)
+	fmt.Fprint(res, "Triggered script: " + scriptFile)
 }
 
 func webhookLog(res http.ResponseWriter, req *http.Request) {
@@ -112,8 +98,13 @@ func webhookLog(res http.ResponseWriter, req *http.Request) {
 		fmt.Fprint(res, "Unauthorized.")
 		return
 	}
+	scriptFile, err := parseScriptFile(req.URL.Path, "/webhook/log/")
+	if err != nil {
+		fmt.Fprint(res, "An error occurred:\n\n" + err.Error())
+		return
+	}
 
-	output, err := readLogFiles()
+	output, err := readLogFiles(scriptFile)
 	if err != nil {
 		fmt.Fprint(res, "An error occurred:\n\n" + err.Error())
 	}
@@ -122,38 +113,59 @@ func webhookLog(res http.ResponseWriter, req *http.Request) {
 }
 
 func authenticateRequest(req *http.Request) bool {
-	if OTP_SECRET == nil {
+	if OtpSecret == nil {
 		// otp authentication disabled
 		return true
 	}
-	return verifyOtp(req.URL.Query().Get("otp"))
+	providedOtp := req.URL.Query().Get("otp")
+	expectedOtp := generateOtp()
+	return expectedOtp == providedOtp
 }
 
-func verifyOtp(otp string) bool {
-	expected := generateOtp()
-	return otp == expected
-}
+func parseScriptFile(urlPath string, endpoint string) (string, error) {
+	scriptFile := strings.TrimPrefix(urlPath, endpoint)
+	scriptFile = strings.TrimRight(scriptFile, "/") // remove trailing slashes if any
 
-func writeLog(log []byte) {
-	generated :=
-		"Script " + SCRIPT + " last executed at:\n\n" +
-		time.Now().Format(time.UnixDate) +
-		"\n\nOutput:\n\n";
-	fullLog := append([]byte(generated), log...)
-	_ = ioutil.WriteFile(OUTPUT_LOG_FILE, fullLog, 0666)
-}
-
-func readLogFiles() (string, error) {
-	// check if log file exists
-	if _, err := os.Stat(OUTPUT_LOG_FILE); os.IsNotExist(err) {
-		return "", errors.New("error: unable to find log file(s)")
+	if _, err := os.Stat(scriptFile); os.IsNotExist(err) {
+		return "", errors.New("error: script file does not exist: " + scriptFile)
 	}
 
-	output, err := ioutil.ReadFile(OUTPUT_LOG_FILE)
+	return scriptFile, nil
+}
+
+func writeLog(script string, output []byte, err error) {
+	errorMsg := ""
+	if err != nil {
+		errorMsg = "\n\nError messages:\n\n" + err.Error()
+	}
+
+	generated :=
+		"Script " + script + " last executed at:\n\n" +
+		time.Now().Format(time.UnixDate) +
+		errorMsg +
+		"\n\nOutput:\n\n"
+	fullLog := append([]byte(generated), output...)
+
+	_ = ioutil.WriteFile(getLogFileName(script), fullLog, 0666)
+}
+
+func readLogFiles(script string) (string, error) {
+	logFile := getLogFileName(script)
+
+	// check if log file exists
+	if _, err := os.Stat(logFile); os.IsNotExist(err) {
+		return "", errors.New("error: could not find log file (has the script been run?)")
+	}
+
+	output, err := ioutil.ReadFile(logFile)
 	if err != nil {
 		return "", err
 	}
 	return string(output), nil
+}
+
+func getLogFileName(scriptFile string) string {
+	return strings.ReplaceAll(scriptFile, ".", "") + ".log"
 }
 
 // never roll your own crypto, but for the sake of keeping this project simple this will do.
@@ -162,7 +174,7 @@ func generateOtp() string {
 	message := make([]byte, 8)
 	binary.BigEndian.PutUint64(message, uint64(curTime))
 
-	hmacSha1 := hmac.New(sha1.New, OTP_SECRET)
+	hmacSha1 := hmac.New(sha1.New, OtpSecret)
 	hmacSha1.Write(message)
 	hash := hmacSha1.Sum(nil)
 
